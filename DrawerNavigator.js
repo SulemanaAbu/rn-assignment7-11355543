b69c81098b3b5ba41e804cd6b5c2f0e5967850c9
SulemanaAbu <151389012+SulemanaAbu@users.noreply.github.com>
2024-07-10 15:10:31 +0000

    -

---
+++ DrawerNavigator.js
@@ -0,0 +1,34 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import CartScreen from './screens/CartScreen';
+import ProductDetailScreen from './screens/ProductDetailScreen';
+
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+const HomeStack = () => (
+    <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="ProductDetail" component={ProductDetailScreen} options={{ title: "Product Details" }} />
+    </Stack.Navigator>
+);
+
+const DrawerNavigator = () => {
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="HomeDrawer">
+                <Drawer.Screen name="HomeDrawer" component={HomeStack} options={{ headerShown: false, title: 'Home' }} />
+                <Drawer.Screen name="Cart" component={CartScreen} />
+                <Drawer.Screen name="Locations" component={CartScreen} />
+                <Drawer.Screen name="Blog" component={CartScreen} />
+                <Drawer.Screen name="Jewelery" component={CartScreen} />
+                <Drawer.Screen name="Electronic" component={CartScreen} />
+                <Drawer.Screen name="Clothing" component={CartScreen} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default DrawerNavigator;
